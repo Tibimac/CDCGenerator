072803b052cd7b0eccc5124289f41bfd72a4a6f3
Juteau François <f.juteau@gmail.com>
2015-09-03 00:28:57 -0700

    - FileGeneratorVC : Add

---
+++ CDCGenerator/Controller/FileGeneratorVC.h
@@ -0,0 +1,13 @@
+//
+//  FileGeneratorVC.h
+//  CDCGenerator
+//
+//  Created by François Juteau on 03/09/2015.
+//  Copyright (c) 2015 Thibault Le Cornec. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface FileGeneratorVC : ViewController
+
+@end
---
+++ CDCGenerator/Controller/FileGeneratorVC.m
@@ -0,0 +1,22 @@
+//
+//  FileGeneratorVC.m
+//  CDCGenerator
+//
+//  Created by François Juteau on 03/09/2015.
+//  Copyright (c) 2015 Thibault Le Cornec. All rights reserved.
+//
+
+#import "FileGeneratorVC.h"
+
+@interface FileGeneratorVC ()
+
+@end
+
+@implementation FileGeneratorVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
