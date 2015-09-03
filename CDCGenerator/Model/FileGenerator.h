5d3f692e341ebf1b927c72fa8494401df4af876e
Juteau François <f.juteau@gmail.com>
2015-09-03 00:05:35 -0700

    - FileGenerator : Add

---
+++ CDCGenerator/Model/FileGenerator.h
@@ -0,0 +1,13 @@
+//
+//  FileGenerator.h
+//  CDCGenerator
+//
+//  Created by François Juteau on 03/09/2015.
+//  Copyright (c) 2015 Thibault Le Cornec. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileGenerator : NSObject
+
+@end
---
+++ CDCGenerator/Model/FileGenerator.m
@@ -0,0 +1,13 @@
+//
+//  FileGenerator.m
+//  CDCGenerator
+//
+//  Created by François Juteau on 03/09/2015.
+//  Copyright (c) 2015 Thibault Le Cornec. All rights reserved.
+//
+
+#import "FileGenerator.h"
+
+@implementation FileGenerator
+
+@end
